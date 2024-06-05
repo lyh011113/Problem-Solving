b44cf219c7fbe4c1248cf377713bf797389f35ed
Yohan Lee <77436799+lyh011113@users.noreply.github.com>
2024-06-05 20:16:25 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main (){
+int T, S;
+
+std::cin >> T >> S;
+
+if(S==1||!(T>=12&&T<=16)){
+    std::cout << 280<<std::endl;
+}else{
+    std::cout << 320<<std::endl;
+}
+
+
+}
