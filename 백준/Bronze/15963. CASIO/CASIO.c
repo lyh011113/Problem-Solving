2511782eaa936c72afe885786ae71ef23ec60089
Yohan Lee <77436799+lyh011113@users.noreply.github.com>
2024-06-13 19:01:43 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    if(n==m){
+        printf("1\n");
+    }else{
+        printf("0\n");
+    }
+
+    return 0;
+}
