01cfb131b8362820f3d23f732f3038ba4a429d16
Yohan Lee <77436799+lyh011113@users.noreply.github.com>
2025-02-06 16:46:44 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,38 +1,50 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <cstdio>
-
-int N[100000];
-int M[100000];
+using namespace std;
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N, M;
+    cin >> N;
+    vector<int> A(N, 0);
+    for (int i = 0; i < N; i++)
     {
-        scanf("%d", &N[i]);
-    }
-    int m;
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++)
-    {
-        scanf("%d", &M[i]);
-    }
-    std::sort(N, N + n);
-
-    for (int i = 0; i < m; i++)
-    {
-        bool isFound = std::binary_search(N, N + n, M[i]);
-        if (isFound)
-        {
-            printf("1\n");
-        }
-        else
-        {
-            printf("0\n");
-        }
+        cin >> A[i];
     }
 
-    return 0;
+    sort(A.begin(), A.end());
+    cin >> M;
+
+    for (int i = 0; i < M; i++)
+    {
+        int num, exist;
+        exist = 0;
+        cin >> num;
+        int start = 0;
+        int end = A.size() - 1;
+        while (start <= end)
+        {
+            int midi = (start + end) / 2;
+            int midV = A[midi];
+
+            if (midV > num)
+            {
+                end = midi - 1;
+            }
+            else if (midV < num)
+            {
+                start = midi + 1;
+            }
+            else
+            {
+                exist = 1;
+                break;
+            }
+        }
+        cout << exist << '\n';
+    }
 }
